255b045a8beed1e8c6d8e6fa246a1f803c0b7373
S. Verona Malone <sverona@sverona.xyz>
2020-12-15 19:48:00 -0500

    fix template

--- template/part1.c
+++ template/part1.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argv, char** argc) {
-	FILE* in = fopen(argc[1], "r");
-
-	int lines = 0;
-	int num;
-	while (fscanf(in, "%d", &num) != EOF) {
-		lines++;
-		printf("%d\n", num);
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s input\n", argv[0]);
+		exit(1);
 	}
+
+	FILE* in = fopen(argv[1], "r");
+
+	char* line = calloc(100, sizeof(char));
+	while (fgets(line, 100, in) != NULL) {
+		printf("%s\n", line);
+	}
+
+	free(line);
 
 	return 0;
 }
